879d53b4c08c6d2d0a653e6c1668c2c031591090
npraderi <npraderi@gmail.com>
2020-03-05 12:26:00 -0300

    Each film has it's comments paged

---
+++ js/filmDetail.js
@@ -0,0 +1,39 @@
+var page = 1;
+var filmId;  
+var comment;    
+function load() {
+    $.ajax({
+
+        url: "../../../src/components/films/commentsWithPagination.php",
+        
+        data: {      
+            pag: page,
+            Id: filmId,
+            comment: comment
+        },
+        type: "GET",
+        dataType: "html"
+    }).done(function (resp) {
+        $("#commentlist").html(resp);
+
+        $("#back").click(function () {
+            page--;
+            load();
+        });
+
+        $("#next").click(function () {
+            page++;
+            load();
+        });
+
+    }).fail(function () {
+        alert("Error 404 not found");
+    });
+}
+$(document).ready(function () {
+    
+    filmId = $("#filmId").val();       
+    comment = $("#comment").val(); 
+    load();
+});
+
